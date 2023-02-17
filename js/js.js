1b96cac3b3d5e2536e98e7860bfc84dd2aa4a9fe
Mirjax2000 <miroslav.viktorin@seznam.cz>
2023-02-17 23:49:26 +0100

    init

--- js/js.js
+++ js/js.js
@@ -1,13 +1,42 @@
 /** @format */
-// $('document').ready(function () {});
-// --- anebo ---
-// $(function () {});
-// --- anebo na izolaci jquery ---
-// (function ($) {
-// })(jQuery);
 
-$(function () {
-    $('.hamburger').on('click', function () {
-        $('.mobile').slideToggle(500);
+/** @format */
+// hamburger menu
+let classDown = document.querySelector('.mobile');
+let hamburgerIcon = document.querySelector('.hamburger');
+
+hamburgerIcon.addEventListener('click', function (event) {
+    classDown.classList.toggle('down');
+});
+
+// expand button
+let expandBttns = document.querySelectorAll('.expand-bttn');
+
+expandBttns.forEach(function (btn) {
+    btn.addEventListener('click', function (event) {
+        let paragraph = btn.parentNode.querySelector('p');
+        paragraph.classList.toggle('expand'); // pridej classu a vypni overflow
+
+        let getComputeStyle = window.getComputedStyle(paragraph); // zjitit overflow
+
+        let gradient = btn.parentNode.querySelector('.gradient'); // aktivovat parrent gradientu
+
+        if (getComputeStyle.overflow === 'auto') {
+            btn.textContent = 'close';
+            btn.style.backgroundColor = 'orange';
+            gradient.style.display = 'none';
+        } else {
+            btn.textContent = 'expand';
+            gradient.style.display = 'block';
+            btn.style.backgroundColor = '#6ed7fe';
+        }
     });
+});
+
+// scroll anim
+let scroll = document.querySelector('.scroll');
+scroll.addEventListener('click', function (event) {
+    let scrolable = document.documentElement.scrollHeight;
+
+    window.scrollTo(0, scrolable);
 });
