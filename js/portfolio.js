b708018c54a0349b943eda137729aaf5663d6b3b
Mirjax2000 <miroslav.viktorin@seznam.cz>
2023-02-26 11:07:18 +0100

    init

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,37 +1,39 @@
 /** @format */
 // hamburger menu
+
 let classDown = document.querySelector('.mobile');
 let hamburgerIcon = document.querySelector('.hamburger');
 
 hamburgerIcon.addEventListener('click', function (event) {
     classDown.classList.toggle('down');
 });
-
 $(document).ready(function () {
+    let time = 500;
+
     $('#frontEnd').click(function () {
         if ($('#frontEndSection').is(':hidden')) {
-            $('section').slideUp(500);
-            $('#frontEndSection').slideDown(500);
+            $('.section').slideUp(time);
+            $('#frontEndSection').slideDown(time);
         } else {
-            $('#frontEndSection').slideUp(500);
+            $('#frontEndSection').slideUp(time);
         }
     });
 
     $('#websites').click(function () {
         if ($('#websitesSection').is(':hidden')) {
-            $('section').slideUp(500);
-            $('#websitesSection').slideDown(500);
+            $('.section').slideUp(time);
+            $('#websitesSection').slideDown(time);
         } else {
-            $('#websitesSection').slideUp(500);
+            $('#websitesSection').slideUp(time);
         }
     });
 
     $('#other').click(function () {
         if ($('#otherSection').is(':hidden')) {
-            $('section').slideUp(500);
-            $('#otherSection').slideDown(500);
+            $('.section').slideUp(time);
+            $('#otherSection').slideDown(time);
         } else {
-            $('#otherSection').slideUp(500);
+            $('#otherSection').slideUp(time);
         }
     });
 });
